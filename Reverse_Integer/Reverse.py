93ced8335ce1e72a40b22fb405f32d84d7e42978
sahar n <saharn@localhost-live.attlocal.net>
2022-08-29 00:19:15 -0400

    New project :))

---
+++ Reverse_Integer/Reverse.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the
+signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+Example 1:
+Input: x = 123
+Output: 321
+Example 2:
+Input: x = -123
+Output: -321
+"""
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Reverse:
+
+    def __int__(self, number): #self
+        self.number=number
+    def reverse(self): #to reverse the given number
+        for i in range(0,32): #loop over the             
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
