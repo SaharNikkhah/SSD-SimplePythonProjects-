ea4fc9d9198b073c2808f2475efc54e75ad89390
sahar n <saharn@localhost-live.attlocal.net>
2022-08-28 22:23:13 -0400

    I did few changes! Thanks for the Code!

--- src/run.py
+++ src/run.py
@@ -1,30 +1,32 @@
-from loguru import logger
+#Third Version SNI(just because we are both SN =))))))))))))))))
+#from loguru import logger#// I Do not know what this is? I will look
 
 
-class TwoSum:
+class TwoSum: # SNI how about merging these?
     def __init__(self, num, target):
         self.num = num
         self.target = target
 
     # def __call__():
 
-    def twosum(self):  # bayad baghei self haram beidm?
-        for index, value in enumerate(self.num):
+    def twosum(self):  # bayad baghei self haram beidm?SNI vagheyat nemidunam =)))))))
+        for index, value in enumerate(self.num):# SNI this was nice I did not know about enumerate
         # for i in range(len(self.num)):  # other methods for this like enumerate
-            # https://www.techiedelight.com/loop-through-list-with-index-python/
-            # for methods of class, self. is needed?
+            # https://www.techiedelight.com/loop-through-list-with-index-python/ //SNI WELL THANK YOU SO MUCH
+            # for methods of class, self. is needed? //SNI I feel like yes!
             # loop over indexing?
-            if (self.target-self.num[index]) in self.num:
-                logger.info(f' [{index}, {self.num.index(abs(self.num[index]-self.target))}]')
-                break
-            else:
-                logger.info(f'not founded!')
-                break
+            sub=self.target-self.num[index]
+            if sub in self.num:
+                return [index, self.num.index(sub)]
 
 if __name__ == '__main__':
-    tmp = TwoSum(num = [2,3,4,5], target = 6)
-    tmp.twosum()
-    logger.info(f'Done!')
+    list=[0,2,4,5]
+    number=6
+    tmp= TwoSum (list,number)
+
+    print (tmp.twosum())
+    #tmp.twosum()
+    #logger.info(f'Done!')
 
 # what other options?
 # the code is not as beautiful as I like to be
