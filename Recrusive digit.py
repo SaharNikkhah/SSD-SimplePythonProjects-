66cd7a5eb22e62a730e67031c187a5a29a2a4ed3
sahar n <saharn@localhost-live.attlocal.net>
2022-09-02 00:35:20 -0400

    new

--- Recrusive digit.py
+++ Recrusive digit.py
@@ -4,7 +4,11 @@
 Given an integer, we need to find the super digit of the integer.
 
 If  has only  digit, then its super digit is .
-Otherwise, the super digit of  is equal to the super digit of the sum of the digits of ."""
+Otherwise, the super digit of  is equal to the super digit of the sum of the digits of
+super_digit(9875)   	9+8+7+5 = 29
+	super_digit(29) 	2 + 9 = 11
+	super_digit(11)		1 + 1 = 2
+	super_digit(2)		= 2 ."""
 import unittest
 
 
